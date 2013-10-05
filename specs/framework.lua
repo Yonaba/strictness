a97b49abadad525c1b81281f34fbabeb02faae8b
Yonaba <roland.yonaba@gmail.com>
2013-10-05 14:34:21 +0000

    Replaced Telescope with a custom made test framework

---
+++ specs/framework.lua
@@ -0,0 +1,101 @@
+-- ==============================================
+-- Custom test framework for specification tests
+-- Copyright (c) 2013 Roland Y., MIT License
+-- ==============================================
+
+local COUNT = 0
+local PASSED = 0
+
+-- Custom assertions
+local assert = function(cond, msg)
+  COUNT = COUNT + 1
+  if not cond then
+    error(msg, 3)
+  end
+end
+
+-- Single test output decorator
+local function output(msg, depth)
+  depth = (depth or 0) + 1
+  PASSED = PASSED + 1
+  msg = ('%s%s'):format(('  '):rep(depth or 1),msg)
+  print(msg .. ('%s[OK]'):format(('.'):rep(76-#msg-1)))
+end
+
+-- ====================
+-- Assertion functions
+-- ====================
+
+-- Test passing when v is nil
+local function assert_nil(msg, v, level)
+  assert(v==nil, ('Test failed, expected %s to be nil')
+    :format(tostring(v)))
+  output(msg, level)
+end
+
+-- Test passing when v is not nil
+local function assert_not_nil(msg, v, level)
+  assert(v~=nil, ('Test failed, expected %s not to be nil')
+    :format(tostring(v)))
+  output(msg, level)
+end
+
+-- Test passing when v is true
+local function assert_true(msg, v, level)
+  assert(v==true, ('Test failed, expected %s to be true')
+    :format(tostring(v)))
+  output(msg, level)
+end
+
+-- Test passing when v is false
+local function assert_false(msg, v, level)
+  assert(v==false, ('Test failed, expected %s to be false')
+    :format(tostring(v)))
+  output(msg, level)
+end
+
+-- Test passing when a == b
+local function assert_equal(msg, a, b, level)
+  assert(a == b, ('Test failed, expected %s to be equal to %s')
+    :format(tostring(a), tostring(b)))
+  output(msg, level)
+end
+
+-- Test passing when a ~= b
+local function assert_not_equal(msg, a, b, level)
+  assert(a ~= b, ('Test failed, expected %s not to be equal to %s')
+    :format(tostring(a), tostring(b)))
+  output(msg, level)
+end
+
+-- Test passing if the call f() produces an error
+local function assert_error(msg, f, level)
+  assert(not pcall(f), 'Expected to create an error')
+  output(msg, level)
+end
+
+-- Test passing if the call f() does not produce an error
+local function assert_not_error(msg, f, level)
+  assert(pcall(f), 'Expected not to create an error')
+  output(msg, level)
+end
+
+-- Prints tests results
+local function print_stats()
+  print(('='):rep(79))
+  print(('Total %d tests : %d passed and %s failed')
+    :format(COUNT, PASSED, COUNT-PASSED))
+end
+
+-- Returns framework
+return {
+  assert_equal = assert_equal,
+  assert_not_equal = assert_not_equal,
+  assert_error = assert_error,
+  assert_not_error = assert_not_error,
+  assert_nil = assert_nil,
+  assert_not_nil = assert_not_nil,
+  assert_true = assert_true,
+  assert_false = assert_false,
+  print_stats = print_stats,
+}
--- specs/tests.lua
+++ specs/tests.lua
@@ -1,290 +1,119 @@
--- ================================================================
--- test suite for strictness
--- Uses Telescope (https://github.com/norman/telescope)
--- ================================================================
+-- ==============================================
+-- Specification test suite for strictness
+-- Uses custom test framework (see framework.lua)
+-- ==============================================
 
--- ================================================================
--- Note :
+-- Test framework
+local test = require 'specs/framework'
 
--- As Telescope declares globals (assertion functions mostly), I 
--- had to track and declare those globals (in a hacky way) to 
--- enable strictness. See context 'Requiring strictness'.
--- ================================================================
-
--- Collects all keys in a given context
-local function collect(t)
-  local r = {}
-  for k in pairs(t) do
-    if not r[k] then r[k] = true end
-  end
-  return r
-end
-
--- Returns diff keys
-local function diff(n, o)
-  local diff = {}
-  for k in pairs(n) do
-    if not o[k] then diff[#diff+1] = k end
-  end
-  return diff
-end
-
--- Collects all existing keys in _G
-local old_G = collect(_G)
+-- Aliases
+local loaded = package.loaded
 
 -- Setting a fake metatable to _G, for the Environment context
 local fake_mt = {
   __index = function() end,
   __newindex = function() end,
   __tostring = function() return "fake_mt" end,
-  a_field = 1,
-  another_field = true,
 }
 
 -- Caching fake_mt field to track changes later
 local old_fake_mt_index = fake_mt.__index
 local old_fake_mt_newindex = fake_mt.__newindex
 local old_fake_mt_a_field = fake_mt.a_field
-local old_fake_mt_another_field = fake_mt.another_field
 
-context('Requiring strictness', function()
+setmetatable(_G, fake_mt)
 
-  test('Declaring test suite globals', function()
-    
-    local globs = diff(_G, old_G)
-    setmetatable(_G, fake_mt)    
-    require 'strictness'
-    
-    for _,var in ipairs(globs) do
-      global (var)
-    end
-    
-    assert_not_nil(global)
-    assert_not_nil(globalize)
-    
-  end)
+-- Creating data for tests
+-- Lua reserved keywords
+local reserved = {
+  'and', 'else', 'false', 'if', 'nil', 'repeat', 'until', 'true',
+  'break', 'elseif', 'for', 'in', 'not', 'return', 'do', 'end',
+  'function', 'local', 'or', 'then', 'while'
+}
 
-end)
+-- Some non-valid identifiers
+local wrong_identifiers = {'5_a', '.e_a', 'a,r', 'e+*z', '$b'}
 
-context('Globals', function()
+-- Output decorators for section headers
+local function decorate(str)
+  local line = ('='):rep(79)
+  return ('\n%s\n%s\n%s'):format(line, str, line)
+end
 
-  test('Assigning value to undeclared global raises an error', function()
-    
-    local function setx() x = true end
-    
-    assert_error(setx)
-    
-  end)
-  
-  test('Assigning nil to undeclared global raises an error', function()
-    
-    local function setx() x = nil end
-    
-    assert_error(setx)
-    
-  end)  
+-- ===============
+-- Running tests
+-- ===============
 
-  test('should be declared before assignment via global()', function()
-    
-    local function setx()
-      global "x"
-      x = true
-    end
-    
-    assert_not_error(setx)
-    assert_true(x)
-    
-  end)
-  
-  test('a newly assigned global takes the value nil by default', function()
-    
-    local function setn()
-      global "n"
-    end
-    
-    assert_not_error(setn)
-    assert_nil(n)
-    
-  end)
-  
-  test('global accepts only strings', function()
-    
-    assert_false(pcall(global,{}))
-    assert_false(pcall(global, 1))
-    assert_false(pcall(global,nil))
-    assert_false(pcall(global,false))
-    assert_false(pcall(global,true))
-    assert_false(pcall(global,function() end))
-    assert_false(pcall(global, coroutine.create(function() end)))
-    
-  end)
-  
-  test('multiple globals can be declared with global()', function()
-    
-    local function declare_multiple()
-      global ("xx", "yy", "zz")
-    end
-    
-    assert_not_error(declare_multiple)
-    assert_nil(xx)
-    assert_nil(yy)
-    assert_nil(zz)
-    
-    
-    local function set_multiple()
-      global ("aa", "bb", "cc")
-      aa, bb, cc = 1, 2, 3
-    end
-    
-    assert_not_error(set_multiple)
-    assert_equal(aa, 1)
-    assert_equal(bb, 2)
-    assert_equal(cc, 3)
-    
-  end)
-  
-  test('global identifiers cannot be any of Lua reserved keywords', function()
-    local reserved = {
-      ['and'] = true,     ['break'] = true,   ['do'] = true,
-      ['else'] = true,    ['elseif'] = true,  ['end'] = true ,
-      ['false'] = true,   ['for'] = true,     ['function'] = true,
-      ['if'] = true,      ['in'] = true,      ['local'] = true ,
-      ['nil'] = true,     ['not'] = true ,    ['or'] = true,
-      ['repeat'] = true,  ['return'] = true,  ['then'] = true ,
-      ['true'] = true ,   ['until'] = true ,  ['while'] = true,
-    }    
-    
-    for var in pairs(reserved) do
-      assert_false(pcall(global,var))
-    end
-    
-  end) 
-  
-  test('they should also respect Lua\'s identifiers lexical conventions', function()  
-    
-    assert_false(pcall(global,"5_a"))
-    assert_false(pcall(global,".a"))
-    assert_false(pcall(global,"e;z"))
-    assert_false(pcall(global,"e+*z"))
-    assert_false(pcall(global,"$b"))
-    
-  end) 
-  
-  test('a declared global can be assigned nil', function()
-    
-    local function setx()
-      global "x"
-      x = true
-    end
-    
-    local function setnil() x = nil end
-    setx()
-    
-    assert_not_error(setnil)
-    assert_nil(x)
-    
-  end)
-  
-end)
+require 'strictness'
 
-context('Functions declaring globals', function()
-  
-  test('raises an error when called normally', function()
-    
-    local function set()
-      global_one = "one"
-      global_two = "two"
-    end
-    
-    assert_error(set)
-    
-  end)
-  
-  test('globalizing such functions no longer raises an error', function()
-    
-    local function set()
-      global_one = "one"
-      global_two = "two"
-    end
-    
-    local set_globalized = globalize(set)
-    
-    assert_error(set)
-    assert_not_error(set_globalized)
-    assert_equal(global_one, "one")
-    assert_equal(global_two, "two")
-    
-  end)
-  
-  test('globalize() also works with functions returning multiple values', function()
-    
-    local add = globalize(function (...)
-      sum = 0
-      for _,v in ipairs({...}) do
-        sum = sum + v
-      end
-      return sum * 2, sum * 3
-    end)
-    
-    assert_not_error(add)
-    assert_equal(sum, 0)
-    
-    local double, triple = add(1,2,3,4,5)
-    
-    assert_equal(sum, 15)
-    assert_equal(double, 30)
-    assert_equal(triple, 45)
-    
-  end)  
-  
-end)
+print(decorate('Requiring strictness:'))
+test.assert_not_nil('defines a global named "global"', global)
+test.assert_not_nil('defines a global named "globalize"', globalize)
+test.assert_equal('global is a function', type(global), 'function')
+test.assert_equal('globalize is a function', type(globalize), 'function')
 
-context('Environment', function()
-  
-  test('preserves a possible existing metatable for _G', function()
-  
-    assert_equal(getmetatable(_G), fake_mt)
+print(decorate('Globals:'))
+test.assert_error('Assigning undeclared global raises an error', function() x = true end)
+test.assert_error('Assigning nil to undeclared global raises an error', function() x = nil end)
+test.assert_not_error('Should be declared before assignment via global()', function() global ("x"); x = true end)
+test.assert_not_nil('declared global x successfully', x, 2)
+test.assert_true('assigned global x successfully', x, 2)
+test.assert_not_error('A newly assigned global takes the value nil by default', function() global ("n"); end)
+test.assert_nil('declared n, n is nil', n, 2)
+test.assert_not_error('A declared global can also be assigned nil', function() n = nil end)
+test.assert_nil('n was assigned nil', n, 2)
+test.assert_error('global() do not accept tables', function() global({}) end)
+test.assert_error('global() do not accept numbers', function() global(1) end)
+test.assert_error('global() do not accept nil', function() global(nil) end)
+test.assert_error('global() do not accept boolean', function() global(true) end)
+test.assert_error('global() do not accept functions', function() global(function() end) end)
+test.assert_error('global() do not accept coroutine, userdata or threads', function() global(coroutine.create(function() end)) end)
+test.assert_not_error('global() only accept strings', function() global("a") end)
+test.assert_error('But not empty strings', function() global("") end)
+test.assert_not_error('Multiple globals can be declared with global()', function() global("yy", "zz") end)
+test.assert_nil('yy was declared, value is nil', yy, 2)
+test.assert_nil('zz was declared, value is nil', zz, 2)
 
-  end)
-  
-  test('but overwrites __index/__newindex in the env table', function()
-    
-    assert_not_equal(fake_mt.__index, old_fake_mt_index)
-    assert_not_equal(fake_mt.__newindex, old_fake_mt_newindex)
-  
-  end)
+print('  Global varnames cannot be a Lua reserved keywords')
+for _, varname in ipairs(reserved) do
+  test.assert_error(('like %s'):format(varname), function() global(varname) end, 2)
+end
 
-  test('other existing fields are preserved', function()
-    
-    assert_equal(fake_mt.a_field, old_fake_mt_a_field)
-    assert_equal(fake_mt.another_field, old_fake_mt_another_field)
-    assert_equal(tostring(_G), 'fake_mt')
-  
-  end)
-  
-  test('strictness can be required even if _G was previously locked', function()
-    
-    package.loaded['strictness'] = nil
-    setmetatable(_G, {__newindex = function() error('Cannot write in _G') end})
-    
-    local function set() var = true end
-    
-    assert_error(var)
-    
-    local globs = diff(_G, old_G)
-    require 'strictness'
-    for _,var in ipairs(globs) do
-      global (var)
-    end
-    
-    local function set() 
-      global "var"
-      var = true 
-    end
-    
-    assert_not_error(set)
-    assert_true(var)
-  
-  end)
-  
-end)
+print('  global() raises an error when identifiers are not valid')
+for _, varname in ipairs(wrong_identifiers) do
+  test.assert_error(('like %s'):format(varname), function() global(varname) end, 2)
+end
+
+print('  Varname "global" can be assigned to a global')
+local glob, globz = _G.global, _G.globalize
+test.assert_not_error('declared global "global"',function() global "global"; global = true end, 2)
+test.assert_true('assigned var "global" a boolean value', global, 2)
+test.assert_not_error('reassigned original global successfully', function() global = glob end, 2)
+
+print('  Varname "globalize" can also be assigned to a global')
+local globz = _G.globalize
+test.assert_not_error('declared global "globalize"',function() global "globalize"; globalize = false end, 2)
+test.assert_false('assigned var "globalize" a boolean value', globalize, 2)
+test.assert_not_error('reassigned original globalize successfully', function() globalize = globz end, 2)
+
+print(decorate('Functions declaring globals:'))
+test.assert_error('Raises error when called normally', function() one = "one" end)
+test.assert_not_error('globalize() wraps these functions so that they can write globals', globalize(function() one = "one" end))
+test.assert_equal('called a function which declared a global successfully',one, "one", 2)
+
+print(decorate('Environment metatable:'))
+test.assert_equal('Preserves a possible existing metatable for _G', getmetatable(_G), fake_mt)
+test.assert_not_equal('Overwrites __index in the env table metatable, if defined', fake_mt.__index, old_fake_mt_index)
+test.assert_not_equal('Overwrites __newindex in the env table metatable, if defined', fake_mt.__newindex, old_fake_mt_newindex)
+test.assert_equal('Any other defined field is preserved in the env metatable', tostring(_G), "fake_mt")
+
+print(decorate('Locked Environments:'))
+test.assert_not_error('Let\'s unload strictness from package.loaded', function() package.loaded['strictness'] = nil end)
+test.assert_not_error('Now we lock _G with a new metatable implemeting a __newindex field', function() setmetatable(_G,{__newindex = function() error() end}) end)
+test.assert_error('We can no longer create globals', function() var = true end)
+test.assert_nil('could not assign variable "var"', var, 2)
+test.assert_not_error('Let\' require strictness', function() require "strictness" end)
+test.assert_not_error('We can now declare globals', function() global "var"; var = true end)
+test.assert_true('successfully declared and assigned global "var" to true', var, 2)
+
+test.print_stats()
