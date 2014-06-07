c0d3f3b5790c48dab737998e25d50be097bee049
Yonaba <roland.yonaba@gmail.com>
2014-06-07 12:51:15 +0000

    New strict rules

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+CHANGELOG
+=========
+
+###0.2.0 (releasedate)
+* `strictness` no longer create globals. It returns a local table of library functions.
+* `strictness` can now create (or convert) strict/sloppy tables (or environnements).
+* Strict/sloppy rules are now applied per table (or environnement).
+* Strict mode enforces variable declarations and complain on undefined fields access/assignment.
+* Tables (or environnements) already existing metatables are preserved, including `__index` and `__newindex` fields.
+* Added `strictness.strict` to convert a normal table (or environnement) to a strict one.
+* Added `strictness.sloppy` to convert a strict table (or environnement) to a normal one.
+* Added `strictness.is_strict` to check if a table (or environnement) is strict.
+* Added `strictness.strictf` to wrap a normal function into a sloppy function.
+* Added `strictness.sloppyf` to wrap a normal function into a strict function.
+* Added `strictness.run_strictf` to run a normal function in strict mode.
+* Added `strictness.run_sloppyf` to run a normal function in sloppy mode.
+* Made compliant with Lua 5.2 new _ENV (although striclty speaking there are no globals in Lua 5.2).
+
+###0.1.0 (10/05/13)
+* Initial release
--- strictness.lua
+++ strictness.lua
@@ -1,142 +1,159 @@
--- ================================================================
--- "strictness" tracks declaration and assignment of globals in Lua
--- Copyright (c) 2013 Roland Y., MIT License
--- v0.1.0 - compatible Lua 5.1, 5.2
--- ================================================================
+-- ==========================================
+-- "strictness", a strict mode for Lua
+-- Copyright (c) 2014 Roland Y., MIT License
+-- v0.2.0 - compatible Lua 5.1, 5.2
+-- ==========================================
 
-local setmetatable = setmetatable
-local getmetatable = getmetatable
-local type = type
-local rawget = rawget
-local rawset = rawset
-local unpack = unpack
-local error = error
-local getfenv = getfenv
-
--- ===================
--- Private helpers
--- ===================
-
--- Detecting Lua52
 local _LUA52 = _VERSION:match('5.2')
+local setmetatable, getmetatable = setmetatable, getmetatable
+local rawget, rawget = rawget, rawget
+local unpack = _LUA52 and table.unpack or unpack
+local tostring, select, error = tostring, select, error
 
 -- Lua reserved keywords
-local luaKeyword = {
-  ['and'] = true,     ['break'] = true,   ['do'] = true,
-  ['else'] = true,    ['elseif'] = true,  ['end'] = true ,
-  ['false'] = true,   ['for'] = true,     ['function'] = true,
-  ['if'] = true,      ['in'] = true,      ['local'] = true ,
-  ['nil'] = true,     ['not'] = true ,    ['or'] = true,
-  ['repeat'] = true,  ['return'] = true,  ['then'] = true ,
-  ['true'] = true ,   ['until'] = true ,  ['while'] = true,
-}
+local is_reserved_keyword = {
+  ['and']      = true, ['break'] = true, ['do']    = true, ['else']   = true, 
+  ['elseif']   = true, ['end']   = true, ['false'] = true, ['for']    = true,
+  ['function'] = true, ['if']    = true, ['in']    = true, ['local']  = true,
+  ['nil']      = true, ['not']   = true, ['or']    = true, ['repeat'] = true, 
+  ['return']   = true, ['then']  = true, ['true']  = true, ['until']  = true,
+  ['while']    = true,
+}; if _LUA52 then is_reserved_keyword['goto'] = true end
 
-if _LUA52 then
-  luaKeyword['goto'] = true
+-- Throws an error if cond
+local function complain_if(cond, msg, level) 
+  return cond and error(msg, level or 3)
 end
 
--- Register for declared globals, defined as a table
--- with weak values.
-local declared_globals = setmetatable({},{__mode = 'v'})
-
--- The global env _G metatable
-local _G_mt
-
--- A custom error function
-local function err(msg, level)  return error(msg, level or 3) end
-
--- Custom assert with error level depth
-local function assert(cond, msg, level)
-  if not cond then
-    return err(msg, level or 4)
+-- Checks if all elements of vararg are valid Lua identifiers
+local function validate_identifiers(...)
+  local varnames = {...}
+  for i, iden in ipairs(varnames) do
+    local is_valid_iden = iden:match('^[%a_]+[%w_]*$') and 
+                          not is_reserved_keyword[iden]
+    complain_if(not is_valid_iden,
+      ('varname #%d "<%s>" is not a valid Lua identifier.')
+        :format(i, tostring(iden)),4)
   end
+  return varnames
 end
 
--- Custom argument type assertion helper
-local function assert_type(var, expected_type, argn, level)
-  local var_type = type(var)
-  assert(var_type == expected_type,
-    ('Bad argument #%d to global (%s expected, got %s)')
-      :format(argn or 1, expected_type, var_type), level)
+-- Swaps keys and values, overwrites values with v if provided
+local function swap_key_and_values(t, v)
+  local _t = {}
+  for i, val in  ipairs(t) do _t[val] = i and v end
+  return _t
 end
 
--- Checks in the register if the given global was declared
-local function is_declared(varname)
-  return declared_globals[varname]
-end
-
--- Checks if the passed-in string can be a valid Lua identifier
-local function is_valid_identifier(iden)
-  return iden:match('^[%a_]+[%w_]*$') and not luaKeyword[iden]
-end
-
--- ==========================
--- Module functions
--- ==========================
-
--- Allows the declaration of passed in varnames
-local function declare_global(...)
-  local vars = {...}
-  assert(#vars > 0,
-    'bad argument #1 to global (expected strings, got nil)')
-  for i,varname in ipairs({...}) do
-    assert_type(varname, 'string',i, 5)
-    assert(is_valid_identifier(varname),
-      ('bad argument #%d to global. "%s" is not a valid Lua identifier')
-        :format(i, varname))
-    declared_globals[varname] = true
+-- Makes a given table strict
+local function make_table_strict(t, ...)
+  t = t or {}
+  local mt = getmetatable(t) or {}
+  complain_if(mt.__strict, 
+    ('<%s> was already made strict.')
+      :format(tostring(t)),3)
+    
+  local varnames = validate_identifiers(...)
+  mt.__allowed = swap_key_and_values(varnames,true)
+  mt.__predefined_index = mt.__index
+  mt.__predefined_newindex = mt.__newindex
+  
+  mt.__index = function(tbl, key)
+    if not mt.__allowed[key] then
+      if mt.__predefined_index then
+        local expected_result = mt.__predefined_index(tbl, key)
+        if expected_result then return expected_result end
+      end
+      complain_if(true,
+        ('Attempt to access undeclared variable "%s" in <%s>.')
+          :format(key, tostring(tbl)),3)
+    end
+    return rawget(tbl, key)
+ end
+  
+  mt.__newindex = function(tbl, key, val)
+    if mt.__predefined_newindex then
+      mt.__predefined_newindex(tbl, key, val)
+      if rawget(tbl, key) ~= nil then return end
+    end
+    if not mt.__allowed[key] then
+      if val == nil then 
+        mt.__allowed[key] = true
+        return
+      end
+      complain_if(not mt.__allowed[key],
+        ('Attempt to assign value to an undeclared variable "%s" in <%s>.')
+          :format(key,tostring(tbl)),3)
+      mt.__allowed[key] = true
+    end
+    rawset(tbl, key, val)
   end
+  
+  mt.__strict = true
+  return setmetatable(t, mt)
+  
 end
 
--- Allows the given function to write globals
-local function declare_global_func(f)
-  assert_type(f, 'function', nil, 5)
+-- Checks if a given table was made strict.
+local function is_table_strict(t)
+  return not not (getmetatable(t) and getmetatable(t).__strict)
+end
+
+-- Makes a given table unstrict
+local function make_table_unstrict(t)
+  if is_table_strict(t) then
+    mt.__index = mt.__predefined_index
+    mt.__newindex = mt.__predefined_newindex
+    mt.__strict = nil
+    mt.__allowed = nil
+    mt.__predefined_index = nil
+    mt.__predefined_newindex = nil
+  end
+  return t
+end
+
+-- Makes a given function strict
+-- Will run un strict mode whether or not env is strict.
+local function make_function_strict(f, ENV)
   return function(...)
-    local old_index, old_newindex = _G_mt.__index, _G_mt.__newindex
-    _G_mt.__index, _G_mt.__newindex = nil, nil
+    local _ENV = ENV or (_LUA52 and _ENV or getfenv(1))
+    local was_patched = is_table_strict(_ENV)
+    if not was_patched then make_table_strict(_ENV) end
     local results = {f(...)}
-    _G_mt.__index, _G_mt.__newindex = old_index, old_newindex
+    if not was_patched then make_table_unstrict(_ENV) end
     return unpack(results)
   end
 end
 
--- ==========================
--- Locking the global env _G
--- ==========================
-
-do
-
-  -- Catches the current env
-  local ENV = _LUA52 and _G or getfenv()
-  
-  -- Preserves a possible existing metatable for the current env
-  _G_mt = getmetatable(ENV)
-  if not _G_mt then
-    _G_mt = {}
-    setmetatable(ENV,_G_mt)
+-- Makes a given function sloppy
+-- Will overrides strict rules of a given env
+local function make_function_sloppy(f,ENV)
+  return function(...)
+    local _ENV = ENV or (_LUA52 and _ENV or getfenv(1))
+    local was_patched = is_table_strict(_ENV)
+    make_table_unstrict(_ENV)
+    local results = {f(...)}
+    if was_patched then make_table_strict(_ENV) end
+    return unpack(results)
   end
+end
 
-  -- Locks access to undeclared globals
-  _G_mt.__index = function(env, varname)
-    if not is_declared(varname) then
-      err(('Attempt to read undeclared global variable "%s"')
-        :format(varname))
-    end
-    return rawget(env, varname)
-  end
+-- Returns the result of function call in strict mode in an env
+local function run_strict(f, env,...)
+  return make_function_strict(f,env)(...)
+end
 
-  -- Locks assignment of undeclared globals
-  _G_mt.__newindex = function(env, varname, value)
-    if not is_declared(varname) then
-      err(('Attempt to assign undeclared global variable "%s"')
-        :format(varname))
-    end
-    rawset(env, varname, value)
-  end
-
+-- Returns the result of function call in sloppy mode in an env
+local function run_sloppy(f, env,...)
+  return make_function_sloppy(f, env)(...)
 end
 
 return {
-  global = declare_global,
-  globalize = declare_global_func
+  strict      = make_table_strict,
+  sloppy      = make_table_unstrict,
+  is_strict   = is_table_strict,  
+  sloppyf     = make_function_sloppy,
+  strictf     = make_function_strict,
+  run_strictf = run_strict,
+  run_sloppyf = run_sloppy,
 }
